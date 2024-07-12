40f5ae943b6c541859fd6a1faf24c323afd49f18
seichi042I <seichi8901@gmail.com>
2024-07-12 13:16:24 +0900

    前処理を別ファイルに分割、kotoba-whisper化、分割処理をexe化

--- utils.py
+++ utils.py
@@ -1,19 +1,41 @@
 import subprocess
 from pathlib import Path
+from pathlib import WindowsPath
+import wave
+import numpy as np
+import os
+import urllib.request
+from tqdm import tqdm
 
 AUDIO_SPLITTER_EXEPATH = "audio_splitter.exe"
+KOTOBA_WHISPER_DIR = "kotoba-whisper"
 TMP_DIRPATH = "./tmp"
 
-def split_audio_on_silence(audio_path, min_silence_len=500, silence_thresh=-40, keep_silence=300):
+def split_audio_on_silence(audio_path:WindowsPath, min_silence_len=500, silence_thresh=-40, keep_silence=300):
     # make directory
     Path(TMP_DIRPATH).mkdir(exist_ok=True, parents=True)
     # execute
-    cmd = [AUDIO_SPLITTER_EXEPATH,"-i", audio_path,"--min-silence-duration",f'"{min_silence_len}"',f"-t={silence_thresh}", "-o", TMP_DIRPATH]
+    cmd = [AUDIO_SPLITTER_EXEPATH,"-i", str(audio_path.absolute()),"--min-silence-duration",f'"{min_silence_len}"',f"-t={silence_thresh}", "-o", TMP_DIRPATH]
     p = subprocess.Popen(' '.join(cmd),stdin=subprocess.PIPE,stderr=subprocess.PIPE)
     out, err = p.communicate()
     print(out)
-    return
+    
+    wavfile_path_list = get_wavfile_path_list("tmp/16kHz")
+    
+    return wavfile_path_list
+
+def get_wavfile_path_list(directory) -> WindowsPath:
+    # 指定されたディレクトリ内の全ファイルとディレクトリのリストを取得
+    files = os.listdir(directory)
+    # .wavファイルのみをフィルタリング
+    wav_files = [(Path(directory) / Path(file)) for file in files if file.endswith('.wav')]
+    # ファイル名でソート
+    wav_files.sort()
+    
+    return wav_files
 
 if __name__ == "__main__":
     print(Path(AUDIO_SPLITTER_EXEPATH).resolve())
-    split_audio_on_silence("./input_wav/test.wav")
+    chunks = split_audio_on_silence("./input_wav/test.wav")
+    print(chunks)
+    
